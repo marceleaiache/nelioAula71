38de7947f03e37743d2eea64a6f03bef732f7320
Marcele Aiache <tuxmarcele@gmail.com>
2023-11-21 23:34:40 -0300

    Upadate

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,8 +5,6 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
-    public static final double PI = 3.14159;
-
     public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
